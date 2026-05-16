dba1ab8eb20f676f632178529e8beece9f74b1fb
Jessica Hamilton <jessica.l.hamilton@gmail.com>
2026-05-16 16:48:15 +1200

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Power Glow Sauce Plugin
+
+This is a plugin for Sauce4Zwift that updates an OBS glow source
+with the colour associated with the current power output of the
+watched athlete.
+
+OBS Source: https://obsproject.com/forum/resources/stroke-glow-shadow.1800/
+Sauce4Zwift: https://github.com/SauceLLC/sauce4zwift
---
+++ pages/src/power-glow-settings.mjs
@@ -0,0 +1,52 @@
+import * as common from '/pages/src/common.mjs';
+
+common.settingsStore.setDefault({
+	zone1: 0,
+	zone2: 0,
+	zone3: 0,
+	zone4: 0,
+	zone5: 0,
+	zone6: 0,
+	zone7: 0,
+	color1: '#ff0000',
+	color2: '#ffa500',
+	color3: '#ffff00',
+	color4: '#008000',
+	color5: '#0000ff',
+	color6: '#4b0082',
+	color7: '#ee82ee'
+});
+
+export const glowSettings = common.settingsStore.get();
+
+console.log(glowSettings);
+
+export async function main() {
+	common.initInteractionListeners();
+	common.subscribe('athlete/watching', updateGlow);
+
+	document.querySelector("#zone1").innerText = glowSettings.zone1;
+	document.querySelector("#zone2").innerText = glowSettings.zone2;
+	document.querySelector("#zone3").innerText = glowSettings.zone3;
+	document.querySelector("#zone4").innerText = glowSettings.zone4;
+	document.querySelector("#zone5").innerText = glowSettings.zone5;
+	document.querySelector("#zone6").innerText = glowSettings.zone6;
+	document.querySelector("#zone7").innerText = glowSettings.zone7;
+
+	document.querySelector("#color1").style.backgroundColor = glowSettings.color1;
+	document.querySelector("#color2").style.backgroundColor = glowSettings.color2;
+	document.querySelector("#color3").style.backgroundColor = glowSettings.color3;
+	document.querySelector("#color4").style.backgroundColor = glowSettings.color4;
+	document.querySelector("#color5").style.backgroundColor = glowSettings.color5;
+	document.querySelector("#color6").style.backgroundColor = glowSettings.color6;
+	document.querySelector("#color7").style.backgroundColor = glowSettings.color7;
+}
+
+export function subscribeWatching(fn) {
+	common.subscribe('athlete/watching', fn);
+}
+
+export async function settingsMain() {
+	common.initInteractionListeners();
+	await common.initSettingsForm('form')();
+}
